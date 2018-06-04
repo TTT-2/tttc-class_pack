fa19c38e4db6bf7664651179a6b25395e866d493
Alf21 <4lf-mueller@gmx.de>
2018-06-04 23:44:22 +0200

    Hotfix

--- lua/autorun/tttc_pack1_alf21.lua
+++ lua/autorun/tttc_pack1_alf21.lua
@@ -109,7 +109,7 @@ if SERVER then
                 "genji_melee"
             },
             items = {
-                EQUIP_BLUE_BULL
+                EQUIP_BLUE_BULL_CUSTOM
             }
         })
         
@@ -120,7 +120,7 @@ if SERVER then
                 "weapon_ttt_detective_lightsaber"
             },
             items = {
-                EQUIP_BLUE_BULL,
+                EQUIP_BLUE_BULL_CUSTOM,
 				EQUIP_NOFALLDMG
             }
         })
