f69a3a5ed48d981fc5dc32b243d06c772248b553
MisterClems <73431903+MisterClems@users.noreply.github.com>
2020-10-30 21:29:12 +0100

    Update class_jedi.lua

--- lua/classes/classes/class_jedi.lua
+++ lua/classes/classes/class_jedi.lua
@@ -11,11 +11,13 @@ CLASS.AddClass("JEDI", {
 		name = {
 			English = "Jedi",
 			Deutsch = "Jedi",
+			French = "Jedi",	
 			Русский = "Джедай"
 		},
 		desc = {
 			English = "May the force be with you!",
 			Deutsch = "Möge die Macht mit dir sein!",
+			French = "Que la force soit avec toi!",	
 			Русский = "Да прибудет с тобой сила!"
 		}
 	}
